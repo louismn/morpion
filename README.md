2edca84b4a114fedff45691e440127e1bb1cebcd
louismn <louis.martindunord@eemi.com>
2018-10-16 14:39:21 +0200

    avancer sur le morpion

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Jeu de morpion créer en Ruby. Réalisé par Priscille de la Tour et Louis Martin du Nord.
 
 À L'initialisation du jeu chaque joueur choisie un pseudo.
-Les régles sont simples, au début de chaque tour, un joueur choisi une case sur laquelle jouée si il arrive à aligner 3 case, il a gagné.
+Les régles sont simples, au début de chaque tour, un des joueurs choisi une case sur laquelle jouée si il arrive à aligner 3 case, il a gagné.
--- morpion.rb
+++ morpion.rb
@@ -1,14 +1,65 @@
-tour = 0
-#on définit les cases du plateau
-class BoardCase< Board
 
-	attr_accessor :empty_case, :num
+$tour = 0
+$all_cases = []
 
+
+class Game
+
+attr_accessor :playing, :draw, :victory, :winner
 
 	def initialize
+		@playing =  true
+		draw = false
+		@victory = false
+	end
+
+def game_status
+
+			if ($all_cases[0].is_x && $all_cases[1].is_x && $all_cases[2].is_x || $all_cases[3].is_x && $all_cases[4].is_x && $all_cases[5].is_x || $all_cases[6].is_x && $all_cases[7].is_x && $all_cases[8].is_x || $all_cases[0].is_x && $all_cases[3].is_x && $all_cases[9].is_x || $all_cases[2].is_x && $all_cases[4].is_x && $all_cases[6].is_x || $all_cases[0].is_x && $all_cases[3].is_x && $all_cases[6].is_x || $all_cases[1].is_x && $all_cases[4].is_x && $all_cases[7].is_x || $all_cases[2].is_x && $all_cases[5].is_x && $all_cases[8].is_x)
+				self.victory 
+				@winner = $player1
+			elsif ($all_cases[0].is_O && $all_cases[1].is_O && $all_cases[2].is_O || $all_cases[3].is_O && $all_cases[4].is_O && $all_cases[5].is_O || $all_cases[6].is_O && $all_cases[7].is_O && $all_cases[8].is_O || $all_cases[0].is_O && $all_cases[3].is_O && $all_cases[9].is_O || $all_cases[2].is_O && $all_cases[4].is_O && $all_cases[6].is_O || $all_cases[0].is_O && $all_cases[3].is_O && $all_cases[6].is_O || $all_cases[1].is_O && $all_cases[4].is_O && $all_cases[7].is_O || $all_cases[2].is_O && $all_cases[5].is_O && $all_cases[8].is_O)
+				self.victory 			
+				@winner = $player2	
+			elsif $tour > 9 
+				self.draw
+			end
+		end
+
+
+		def victory
+			@playing = false
+			@victory = true
+		end
+
+		def draw
+			@playing = false
+			@victory = false
+
+		end
+end 
+
+class Board
+
+		def display(case_1,case_2,case_3,case_4,case_5,case_6,case_7,case_8,case_9)
+
+			puts "|#{case_1.display}|#{case_2.display}|#{case_3.display}|"
+			puts "|#{case_4.display}|#{case_5.display}|#{case_6.display}|"
+			puts "|#{case_7.display}|#{case_8.display}|#{case_9.display}|"
+		
+		end	
+end
+
+
+class Boardcase < Board
+
+	attr_accessor :is_x, :is_O, :empty_case
+
+	def initialize(num)
 		@empty_case = true
 		@num = num
-		@all_cases << self
+		$all_cases << self
+		
 	end	
 
 	def display
@@ -19,7 +70,7 @@ class BoardCase< Board
 		elsif self.is_O
 			return "O"
 		end
-	end
+	
 
 end
 
@@ -33,107 +84,56 @@ end
 	@is_O = true
 	end
 
-	
-
-	
-end
-=end
-#on définit l'état du plateau
-class Board
-
-	attr_accessor :cases
-
-		def initialize 
-			@all_cases = [case1,case2,case3,case4,case5,case6,case7,case8,case9,]
-		end
-
-		def to_s
-
-			puts "|#{case1}|#{case2}|#{case3}|"
-			puts "|#{case4}|#{case5}|#{case6}|"
-			puts "|#{case7}|#{case8}|#{case9}|"
 		
-		end
-
-		def play
-
-			 #TO DO : une méthode qui change la BoardCase jouée en fonction de la valeur du joueur (X, ou O)
-		end
-
-		def game_status
-
-			if @all_cases[0].is_x && @all_cases[1].is_x && @all_cases[2].is_x || @all_cases[3].is_x && @all_cases[4].is_x && @all_cases[5].is_x || @all_cases[6].is_x && @all_cases[7].is_x && @all_cases[8].is_x || @all_cases[0].is_x && @all_cases[3].is_x && @all_cases[9].is_x || @all_cases[2].is_x && @all_cases[4].est_x && @all_cases[6].is_x || @all_cases[0].is_x && @all_cases[3].is_x && @all_cases[6].is_x @all_cases[1].is_x && @all_cases[4].is_x && @all_cases[7].is_x @all_cases[2].is_x && @all_cases[5].is_x && @all_cases[8].is_x
-				@victory == true 
-				puts "Le joueur 1 à gagné"
-			elsif @all_cases[0].is_O && @all_cases[1].is_O && @all_cases[2].is_O || @all_cases[3].is_O && @all_cases[4].is_O && @all_cases[5].is_O || @all_cases[6].is_O && @all_cases[7].is_O && @all_cases[8].is_O || @all_cases[0].is_O && @all_cases[3].is_O && @all_cases[9].is_O || @all_cases[2].is_O && @all_cases[4].is_O && @all_cases[6].is_O || @all_cases[0].is_O && @all_cases[3].is_O && @all_cases[6].is_O || all_cases[1].is_O && @all_cases[4].is_O && @all_cases[7].is_O || @all_cases[2].is_O && @all_cases[5].is_O && @all_cases[8].is_O
-				@victory == true				
-				puts "Le joueur 2 à gagné"			
-			elsif tour > 9 && @victory == false
-				self.@draw
-			end
-		end
-		def victory
-			@playing =
 end
 
-class Player
-	attr_accessor :joueur, :value 
-	attr_writer : state
+partie = Game.new
+board = Board.new
 
-	def initialize
-		@joueur = joueur
-		@value = value
-		@state = state
-	end
-end
-
-
-	
-
-
-
-class Game
-
-attr_accessor :playing, :draw, :victory :
-
-	def initialize
-		@player1 = player1
-		@player2 = player2
-		@playing =  true
-		@draw = false
-		@victory = false
-	end
-
-
-	
-	def turn
-		if @playing == true 
-			if tour ‰ 2 == 0 
-				puts "c est à #{$joueur1} de jouer, sur quelle case veux tu jouer ?"
-				$player_entrance = gets.chomp.to_i
-				#créer une fonction qui verifie que la case soit valable 
-			else
-				puts "c est à #{$joueur2} de jouer sur quelle case veux tu jouer ? "
-				$player_entrance = gets.chomp.to_i
-			end
-		
-	
-		elsif @victoire == true
-			puts "La partie est finit le #{}"
-		else @match_nul == true
-			puts "La partie est finie aucun joueur n'a finis à ce départager "
-		end
-
-	end
-
-end  
+case_1,case_2,case_3,case_4,case_5,case_6,case_7,case_8,case_9 = Boardcase.new(1), Boardcase.new(2), Boardcase.new(3), Boardcase.new(4), Boardcase.new(5), Boardcase.new(6), Boardcase.new(7), Boardcase.new(8), Boardcase.new(9)
 
 
 
 puts "Bonjour Bienvenue dans cette partie de morpion, quel est le nom du joueur1 ?"
-$joueur1 = gets.chomp
+$player1 = gets.chomp
 puts "Quel est le nom du joueur 2 ?"
-$joueur2 = gets.chomp
-=end
+$player2 = gets.chomp
+
+while partie.playing
+
+	puts "------------- TOUR NUMÉRO #{$tour} -------------"
+	board.display(case_1,case_2,case_3,case_4,case_5,case_6,case_7,case_8,case_9)
+
+	if $tour % 2 == 0
+		puts "C'est à #{$player1}"
+		player_choice = gets.chomp.to_i
+		while player_choice < 1 || player_choice > 9 || $all_cases[player_choice-1].empty_case == false
+			puts "Entre 1 et 10 stp. Et évite de sélectionner une case déjà prise ;)"
+			player_choice = gets.chomp.to_i
+			end
+			$all_cases
+			$all_cases[player_choice.to_i-1].change_to_O
+	else
+		puts "C'est à #{$player2} de jouer"
+		player_choice = gets.chomp.to_i
+		while player_choice < 1 || player_choice > 9 || $all_cases[player_choice-1].empty_case == false
+			puts "Entre 1 et 10 stp. Et évite de sélectionner une case déjà prise ;)"
+			player_choice = gets.chomp.to_i
+		end
+
+		$all_cases[player_choice.to_i-1].change_to_x
+	end
+	$tour += 1
+	partie.game_status
+	puts $all_cases[0].is_x && $all_cases[1].is_x && $all_cases[2].is_x
+end
+
+
+board.display(case_1,case_2,case_3,case_4,case_5,case_6,case_7,case_8,case_9)
+if partie.victory
+	puts "La partie est terminée !!!! Bravo à #{partie.winner} d'avoir gagné ;)"
+elsif partie.draw
+	puts "C'est dommage, #{$player1} et #{$player2} n'ont pas réussi à se départager. C'est un match nul :("
+end
 
 
