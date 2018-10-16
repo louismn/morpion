967f1df7222650949cd4629e27cbed40a6e6f950
louismn <louis.martindunord@eemi.com>
2018-10-16 12:03:55 +0200

    Avancer sur le morpion

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# morpion
+# Jeu de morpion créer en Ruby. Réalisé par Priscille de la Tour et Louis Martin du Nord.
+
+À L'initialisation du jeu chaque joueur choisie un pseudo.
+Les régles sont simples, au début de chaque tour, un joueur choisi une case sur laquelle jouée si il arrive à aligner 3 case, il a gagné.
--- morpion.rb
+++ morpion.rb
@@ -7,28 +7,94 @@ class BoardCase
 
 	def initialize
 		@case_value = case_value
-		@case_number = case_number	
+		@case_number = case_number
 	end	
 
-	def etat_case
-		
-		if $player_entrance == @case_value 
-		end
+	def to_s
+		case_value = case_value.to_s
 	end
 
-	def to_s
-		case_value
+end
+
+=begin
+	def change_to_x
+	 player_entrance = "x"
+	end
+
+	def change_to_O
+	player_entrance = "O"
+	end
+
+	def affichage
+		if self
+
+	end
+
+	
+end
+=end
+
+class Board
+
+	attr_accessor :cases
+
+		def initialize 
+			@toutes_les_cases = [case1,case2,case3,case4,case5,case6,case7,case8,case9,]
+		end
+
+		def to_s
+
+			puts "|#{case1}|#{case2}|#{case3}|"
+			puts "|#{case4}|#{case5}|#{case6}|"
+			puts "|#{case7}|#{case8}|#{case9}|"
+		
+		end
+
+		def play
+
+			 #TO DO : une méthode qui change la BoardCase jouée en fonction de la valeur du joueur (X, ou O)
+		end
+
+		def victory?
+
+			if @toutes_les_cases[0].est_x && @toutes_les_cases[1].est_x && @toutes_les_cases[2].est_x || @toutes_les_cases[3].est_x && @toutes_les_cases[4].est_x && @toutes_les_cases[5].est_x || @toutes_les_cases[6].est_x && @toutes_les_cases[7].est_x && @toutes_les_cases[8].est_x || @toutes_les_cases[0].est_x && @toutes_les_cases[3].est_x && @toutes_les_cases[9].est_x || @toutes_les_cases[2].est_x && @toutes_les_cases[4].est_x && @toutes_les_cases[6].est_x || @toutes_les_cases[0].est_x && @toutes_les_cases[3].est_x && @toutes_les_cases[6].est_x @toutes_les_cases[1].est_x && @toutes_les_cases[4].est_x && @toutes_les_cases[7].est_x @toutes_les_cases[2].est_x && @toutes_les_cases[5].est_x && @toutes_les_cases[8].est_x
+				@victory == true 
+				puts "Le joueur 1 à gagné"
+			elsif @toutes_les_cases[0].est_O && @toutes_les_cases[1].est_O && @toutes_les_cases[2].est_O || @toutes_les_cases[3].est_O && @toutes_les_cases[4].est_O && @toutes_les_cases[5].est_O || @toutes_les_cases[6].est_O && @toutes_les_cases[7].est_O && @toutes_les_cases[8].est_O || @toutes_les_cases[0].est_O && @toutes_les_cases[3].est_O && @toutes_les_cases[9].est_O || @toutes_les_cases[2].est_O && @toutes_les_cases[4].est_O && @toutes_les_cases[6].est_O || @toutes_les_cases[0].est_O && @toutes_les_cases[3].est_O && @toutes_les_cases[6].est_O @toutes_les_cases[1].est_O && @toutes_les_cases[4].est_O && @toutes_les_cases[7].est_O @toutes_les_cases[2].est_O && @toutes_les_cases[5].est_O && @toutes_les_cases[8].est_O
+				@victory == true 
+				puts "Le joueur 2 à gagné"
+			elsif tour > 9
+				@victory == false
+				@draw == true
+			end
+end
+
+class Player
+	attr_accessor :joueur, :value 
+	attr_writer : state
+
+	def initialize
+		@joueur = joueur
+		@value = value
+		@state = state
 	end
 end
 
-class Jeu 
 
-attr_accessor :playing, :match_nul, :victoire 
+	
+
+
+
+class Game
+
+attr_accessor :playing, :draw, :victory :
 
 	def initialize
+		@player1 = player1
+		@player2 = player2
 		@playing =  true
-		@match_nul = false
-		@victoire = false
+		@draw = false
+		@victory = false
 	end
 
 
@@ -55,27 +121,12 @@ attr_accessor :playing, :match_nul, :victoire
 
 end  
 
-class Board
 
-	attr_accessor :cases
-
-		def initialize 
-			@cases = [case1,case2,case3,case4,case5,case6,case7,case8,case9,]
-		end
-
-		def presentation
-
-			puts "|#{case1}|#{case2}|#{case3}|"
-			puts "|#{case4}|#{case5}|#{case6}|"
-			puts "|#{case7}|#{case8}|#{case9}|"
-		end
-
-
-end
 
 puts "Bonjour Bienvenue dans cette partie de morpion, quel est le nom du joueur1 ?"
 $joueur1 = gets.chomp
 puts "Quel est le nom du joueur 2 ?"
 $joueur2 = gets.chomp
+=end
 
 
